55fa56090456b5d4d829c14b714b399e8e1f9b14
Logan Keller <logan.keller.2023@student.lhschools.org>
2020-03-03 12:23:02 -0500

    keepContainedMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -16,7 +16,42 @@ public class Main {
 
         System.out.println("Name a positive number that is 4 digits long");
         number = keyboard.nextInt();
+                value =10;
+                System.out.println("Name a positive number");
+                number= keyboard.nextInt();
+
+
+                number = 1;
+                System.out.println((number%5)+4);
+
+                number = 2;
+                System.out.println((number%5)+4);
+
+                number = 3;
+                System.out.println((number%5)+4);
 
         System.out.println("The last digit of number is: "+number%10);
+                System.out.println((number%5)+4);
+
+                number = 5;
+                System.out.println((number%5)+4);
+
+                number = 1;
+                System.out.println((number%5)+4);
+
+                number = 2;
+                System.out.println((number%5)+4);
+
+                number = 3;
+                System.out.println((number%5)+4);
+
+                number = 4;
+                System.out.println((number%5)+4);
+
+                number = 5;
+                System.out.println((number%5)+4);
+
+
+                System.out.println("number contained is: " + value);
     }
 }
